b609496d9f105039fef0aad56ef8195087d3c6d5
pima <pima@3eclipses.com>
2012-01-10 00:13:00 +0100

    creating symlinks to default openssh rsa files if -c option is passed

--- 00scripts/lib/ec1/supervision/utils/certificates.rb
+++ 00scripts/lib/ec1/supervision/utils/certificates.rb
@@ -17,6 +17,8 @@ def create(path, name, passphrase_code, default_cert_file_option, passphrase=nil
   case default_cert_file_option
   when '-c'
     openssh_defcert_file="#{path}/user.openssh_defcert"
+    # TODO: use ruby symlink creation command instead of system util
+    system("(cd #{path} ; ln -s #{name} id_rsa ; ln -s #{name}.pub id_rsa.pub)")
   when '-b'
     echo "INFO: bypassing openssh_defcert_file creation"
   else
