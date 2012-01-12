af38468a773c4da2c5bb9c9fb9a3b187732ec59a
pima <pima@3eclipses.com>
2012-01-12 22:50:44 +0100

    lazy live_debug redux

--- 00scripts/lib/ec1/supervision/clusters.rb
+++ 00scripts/lib/ec1/supervision/clusters.rb
@@ -91,7 +91,7 @@ end
 
 def dispatch_ini_ering_data()
   imported_ini_ering_data = { 
-                             ec1_machine_ssh_port = {
+                             ec1_machine_ssh_port => {
                                                     :import_value => EC1_MACHINE_SSH_PORT,
                                                     :dummy_text_replace => '@@_ec1_machine_ssh_port_@@',
                                                     :file_relative_path => 'dispatch/system/00data/machine.ssh_port'
