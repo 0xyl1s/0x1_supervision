9eb8d544ce2572cff06e885d1159c30b83d7b0a7
pima <pima@3eclipses.com>
2012-01-12 16:00:32 +0100

    mainly new util e.cluster_install_ering.cc01_ini

--- 00scripts/lib/ec1/supervision/certificates.rb
+++ 00scripts/lib/ec1/supervision/certificates.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # tested with ruby 1.9.3
 
-module Ec1 module Supervision module Utils module Security
+module Ec1 module Supervision module Security
 
 class Certificates
 require 'ec1/lib/toolkit/standard.rb'
@@ -36,35 +36,15 @@ end
 
 end
 
-end end end end
+end end end
 
 
-# Project infos >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>># {{{
-# Project: Epiculture
-# Author: Pierre-Mael Cretinon
-# Email: projects2011@3eclipses.com
-# coding style: 0.0.2
-# License: GNU GPLv3
-#
-# Notes:
-#
-# License details:
-# <copyright/copyleft>
-# Copyright 2010-2011 (c) Pierre-Mael CRETINON <copyleft@pierremael.net>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
-# </copyright/copyleft>
-# Project infos <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<# }}}
-#vim: foldmethod=marker
+# ____________________________________________________________________
+# >>>>>  projet epiculture/ec1   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#{{{
+# Sources, Infos & Contact : http://www.epiculture.org
+# Author: Pierre-Maël Crétinon
+# License: GNU GPLv3 ( www.epiculture.org/ec1/LICENSE )
+# Copyright: 2010-2012 Pierre-Maël Crétinon
+# Sponsor: studio Helianova - http://studio.helianova.com
+# ――――――――――――――――――――――――――――――――――――――#}}}
+# vim: ft=ruby
---
+++ 00scripts/lib/ec1/supervision/clusters.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+# tested with ruby 1.9.3
+
+module Ec1 module Supervision module Clusters
+
+OSES = [
+       'ub10.04_x86_64',
+       'ub11.04_x86_64'
+]
+
+CLUSTER_TYPES = [
+                'cluster.core',
+                'cluster.leaf'
+                ]
+
+CLUSTER_TYPE_SHORTNAMES = {
+                          'cluster.core' => 'cc',
+                          'cluster.leaf' => 'cf'
+                          }
+
+class Clusters
+require 'ec1/lib/toolkit/standard.rb'
+include Ec1::Lib::Toolkit::Standard
+
+def initialize()
+end
+
+def install(os, cluster_type, ering_version)
+  abort "ERROR: invalid os type (#{os}" unless valid_os?(os)
+  @os = os
+  abort "ERROR: invalid cluster_type (#{cluster_type}" unless valid_os?(os)
+  @cluster_type = cluster_type if valid_cluster_type?(cluster_type)
+  @cluster_type_shortname = cluster_type_shortname(@cluster_type)
+  @supervision_new_cluster_ini_basedir = File.expand_path("~/.ec1.sup/cluster.new")
+  abort "ERROR: when starting new cluster installation, supervision_new_cluster_ini_basedir should be empty (#{@supervision_new_cluster_ini_basedir})"
+  @ering_version = ering_version
+end
+
+
+private
+
+def download_raw_install_ini_dir
+  ering_uri = "https://raw.github.com/epiculture/ec1_supervision_templates/master/#{@os}/erings/#{@cluster_type}/ering.#{@cluster_type_shortname}#{@ering_version}"
+  e__http_download_and_save(ering_uri, @supervision_new_cluster_ini_basedir)
+  ini_dir_archive_uri = "https://raw.github.com/epiculture/ec1_supervision_templates/master/#{@os}/erings/#{@cluster_type}/ini_dir.ec1template.tar"
+  e__http_download_and_save(ini_dir_archive_uri, @supervision_new_cluster_ini_basedir)
+end
+
+def valid_os?(os)
+  e__array_value_exist?(OSES, os)
+end
+
+def valid_cluster_type?(cluster_type)
+  e__array_value_exist?(CLUSTER_TYPES, cluster_type)
+end
+
+def cluster_type_shortname(cluster_type)
+  CLUSTER_TYPE_SHORTNAMES[cluster_type]
+end
+
+end
+
+end end end
+
+
+# ____________________________________________________________________
+# >>>>>  projet epiculture/ec1   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#{{{
+# Sources, Infos & Contact : http://www.epiculture.org
+# Author: Pierre-Maël Crétinon
+# License: GNU GPLv3 ( www.epiculture.org/ec1/LICENSE )
+# Copyright: 2010-2012 Pierre-Maël Crétinon
+# Sponsor: studio Helianova - http://studio.helianova.com
+# ――――――――――――――――――――――――――――――――――――――#}}}
+# vim: ft=ruby
