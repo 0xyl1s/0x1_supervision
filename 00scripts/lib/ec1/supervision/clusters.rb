4a793dfba56f5f13fa6d2bc80caf7f0210e5d3f6
pima <pima@3eclipses.com>
2012-01-14 22:04:28 +0100

    testing x11 programs launch

--- 00scripts/lib/ec1/supervision/clusters.rb
+++ 00scripts/lib/ec1/supervision/clusters.rb
@@ -261,6 +261,9 @@ def remote_execute()
     if e__service_online?(EC1_MACHINE_TEMP_IP, EC1_MACHINE_TEMP_SSH_PORT)
       puts "#{@ec1_log_prefix} starting remote phase_system installation: \n#{ering_ini_ssh_root_phases_command}\n\n"
       puts "#{@ec1_log_prefix} to follow the main live log, run: \n#{ssh_root_temp_command} tail -f '\~/.ec1.ini.ering/logs/ec1.ini.system.ering'\n\n"
+      system "/usr/bin/xcalc"
+      system "/usr/bin/urxvt"
+      abort
       %x"urxvt -e '#{ering_ini_ssh_root_phases_command}'"
       @ering_ini_ssh_root_phases_command_executed = true
     else
